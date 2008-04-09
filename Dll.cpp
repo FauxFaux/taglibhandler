be8823987c5421b867d3a548579cc224f1412fd3
Chris West (Faux) <git@goeswhere.com>
2008-04-09 22:35:02 +0000

    Existing working code for the start of this project, taken from revision 56 of local Vogg tree.
    Requires (at least?) revision 442 of my custom taglib: http://taglibhandler.sourceforge.net/taglib-asf.bzr/

---
+++ Dll.cpp
@@ -0,0 +1,280 @@
+﻿#include <shlwapi.h> // Registry helper APIs
+#include <shlobj.h>  // SHChangeNotify
+
+// {66AC53F1-EB5D-4af9-861E-E3AE9C07EF14}
+#define SZ_CLSID_US           L"{66AC53F1-EB5D-4af9-861E-E3AE9C07EF14}"
+static const GUID CLSID_US =
+{ 0x66ac53f1, 0xeb5d, 0x4af9, { 0x86, 0x1e, 0xe3, 0xae, 0x9c, 0x7, 0xef, 0x14 } };
+
+// Registry path strings
+#define SZ_APPROVEDSHELLEXTENSIONS        L"Software\\Microsoft\\Windows\\CurrentVersion\\Shell Extensions\\Approved"
+#define SZ_TAGLIBPROPERTYHANDLER           L"TagLib Property Handler"
+
+// Reference count for the DLL
+LONG g_cLocks = 0;
+
+// Handle the the DLL's module
+HMODULE g_hmodThis;
+
+// Standard DLL functions
+BOOL APIENTRY DllMain(HINSTANCE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+	g_hmodThis = (HMODULE)hModule;
+
+	return TRUE;
+}
+
+STDAPI DllCanUnloadNow()
+{
+	// Only allow the DLL to be unloaded after all outstanding references have been released
+	return (g_cLocks == 0) ? S_OK : S_FALSE;
+}
+
+void DllAddRef()
+{
+	// Increment the reference count on the DLL
+	InterlockedIncrement(&g_cLocks);
+}
+
+void DllRelease()
+{
+	// Decrement the reference count on the DLL
+	InterlockedDecrement(&g_cLocks);
+}
+
+
+// Constructor for CTagLibPropertyStore
+HRESULT CTagLibPropertyStore_CreateInstance(REFIID riid, void **ppv);
+
+// Class factory for CTagLibPropertyHandler
+class CTagLibPropertyHandlerClassFactory  : public IClassFactory
+{
+public:
+	// IUnknown methods
+	IFACEMETHODIMP QueryInterface(REFIID riid, void **ppv)
+	{
+		static const QITAB qit[] = {
+			QITABENT(CTagLibPropertyHandlerClassFactory, IClassFactory),
+			{ 0 },
+		};
+		return QISearch(this, qit, riid, ppv);
+	}
+
+	IFACEMETHODIMP_(ULONG) AddRef()
+	{
+		DllAddRef();
+		return 2; // Object's lifetime is tied to the DLL, so no need to track its refcount
+	}
+
+	IFACEMETHODIMP_(ULONG) Release()
+	{
+		DllRelease();
+		return 1; // Object's lifetime is tied to the DLL, so no need to track its refcount or manually delete it
+	}
+
+	// IClassFactory methods
+	IFACEMETHODIMP CreateInstance(IUnknown *pUnkOuter, REFIID riid, void **ppv)
+	{
+		// Aggregation is not supported
+		return pUnkOuter ? CLASS_E_NOAGGREGATION : CTagLibPropertyStore_CreateInstance(riid, ppv);
+	}
+
+	IFACEMETHODIMP LockServer(BOOL fLock)
+	{
+		if (fLock)
+			DllAddRef();
+		else
+			DllRelease();
+
+		return S_OK;
+	}
+};
+
+// Single global instance of the class factory object
+CTagLibPropertyHandlerClassFactory g_cfDebugPropertyHandler;
+
+// Export called by CoCreateInstance to obtain a class factory for the specified CLSID
+STDAPI DllGetClassObject(REFCLSID clsid, REFIID riid, void **ppv)
+{
+	if (clsid == CLSID_US)
+		return g_cfDebugPropertyHandler.QueryInterface(riid, ppv);
+
+	return CLASS_E_CLASSNOTAVAILABLE;
+}
+
+// A struct to hold the information required for a registry entry
+struct REGISTRY_ENTRY
+{
+	HKEY    hkeyRoot;
+	LPCWSTR pszKeyName;
+	LPCWSTR pszValueName;
+	DWORD   dwType;
+	LPCWSTR pszData;
+	DWORD   dwData;
+};
+
+
+// Creates a registry key (if needed) and sets the default value of the key
+HRESULT CreateRegKeyAndSetValue(const REGISTRY_ENTRY &pRegistryEntry)
+{
+	HRESULT hr;
+
+	// create the key, or obtain its handle if it already exists
+	HKEY hKey;
+	LONG lr = RegCreateKeyExW(pRegistryEntry.hkeyRoot,
+		pRegistryEntry.pszKeyName,
+		0,
+		NULL,
+		REG_OPTION_NON_VOLATILE,
+		KEY_ALL_ACCESS,
+		NULL,
+		&hKey,
+		NULL);
+
+	hr = HRESULT_FROM_WIN32(lr);
+
+	if (SUCCEEDED(hr))
+	{
+		// extract the data from the struct according to its type
+		LPBYTE pData = NULL;
+		DWORD cbData = 0;
+		hr = S_OK;
+		switch (pRegistryEntry.dwType)
+		{
+		case REG_SZ:
+			pData = (LPBYTE) pRegistryEntry.pszData;
+			cbData = ((DWORD) wcslen(pRegistryEntry.pszData) + 1) * sizeof(WCHAR);
+			break;
+
+		case REG_DWORD:
+			pData = (LPBYTE) &pRegistryEntry.dwData;
+			cbData = sizeof(pRegistryEntry.dwData);
+			break;
+
+		default:
+			hr = E_INVALIDARG;
+			break;
+		}
+
+		if (SUCCEEDED(hr))
+		{
+			// attempt to set the value
+			lr = RegSetValueExW(hKey,
+				pRegistryEntry.pszValueName,
+				0,
+				pRegistryEntry.dwType,
+				pData,
+				cbData);
+			hr = HRESULT_FROM_WIN32(lr);
+		}
+
+		RegCloseKey(hKey);
+	}
+
+	return hr;
+}
+
+// Registers this COM server
+STDAPI DllRegisterServer()
+{
+	HRESULT hr = E_FAIL;
+
+	WCHAR szModuleName[MAX_PATH];
+
+	if (!GetModuleFileNameW(g_hmodThis, szModuleName, ARRAYSIZE(szModuleName)))
+		hr = HRESULT_FROM_WIN32(GetLastError());
+	else
+	{
+		// List of property-handler specific registry entries we need to create
+		static const REGISTRY_ENTRY rgRegistryEntries[] =
+		{
+			// COM information
+			{
+				HKEY_CLASSES_ROOT,
+					L"CLSID\\" SZ_CLSID_US,
+					NULL,
+					REG_SZ,
+					SZ_TAGLIBPROPERTYHANDLER,
+					0
+			},
+			{
+				HKEY_CLASSES_ROOT,
+					L"CLSID\\" SZ_CLSID_US,
+					L"ManualSafeSave",
+					REG_DWORD,
+					NULL,
+					1
+			},
+			{
+				HKEY_CLASSES_ROOT,
+					L"CLSID\\" SZ_CLSID_US L"\\InProcServer32",
+					NULL,
+					REG_SZ,
+					szModuleName,
+					0
+			},
+			{
+				HKEY_CLASSES_ROOT,
+					L"CLSID\\" SZ_CLSID_US L"\\InProcServer32",
+					L"ThreadingModel",
+					REG_SZ,
+					L"Apartment",
+					0
+			},
+
+			// Shell information
+			{
+				HKEY_LOCAL_MACHINE,
+					SZ_APPROVEDSHELLEXTENSIONS,
+					SZ_CLSID_US,
+					REG_SZ,
+					SZ_TAGLIBPROPERTYHANDLER,
+					0
+			}
+		};
+
+		hr = S_OK;
+		for (int i = 0; i < ARRAYSIZE(rgRegistryEntries) && SUCCEEDED(hr); i++)
+		{
+			hr = CreateRegKeyAndSetValue(rgRegistryEntries[i]);
+			if (FAILED(hr))
+				return hr;
+		}
+
+		// inform Explorer, et al of the new handler
+		SHChangeNotify(SHCNE_ASSOCCHANGED, SHCNF_IDLIST, 0, 0);
+	}
+
+	return hr;
+}
+
+
+//
+// Unregisters this COM server
+//
+STDAPI DllUnregisterServer()
+{
+	HRESULT hr = S_OK;
+
+	// attempt to delete everything, even if some operations fail
+	DWORD dwCLSID =           SHDeleteKeyW(HKEY_CLASSES_ROOT, L"CLSID\\" SZ_CLSID_US);
+	DWORD dwShellExtension =  SHDeleteValueW(HKEY_LOCAL_MACHINE, SZ_APPROVEDSHELLEXTENSIONS, SZ_CLSID_US);
+
+	// return first error encountered as HRESULT
+	if (dwCLSID != ERROR_SUCCESS)
+	{
+		hr = HRESULT_FROM_WIN32(dwCLSID);
+	}
+	else if (dwShellExtension != ERROR_SUCCESS)
+	{
+		hr = HRESULT_FROM_WIN32(dwShellExtension);
+	}
+
+	if (SUCCEEDED(hr))
+	{
+		// inform Explorer, et al that the handler has been unregistered
+		SHChangeNotify(SHCNE_ASSOCCHANGED, SHCNF_IDLIST, 0, 0);
+	}
+
+	return hr;
+}
---
+++ TagLibHandler.cpp
@@ -0,0 +1,318 @@
+//
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+//
+
+#define NOMINMAX
+
+#include <string>
+
+#include <initguid.h>
+#include <mmdeviceapi.h>
+
+#include <shobjidl.h>    // IInitializeWithStream, IDestinationStreamFactory
+#include <propsys.h>     // Property System APIs and interfaces
+#include <propkey.h>     // System PROPERTYKEY definitions
+#include <propvarutil.h> // PROPVARIANT and VARIANT helper APIs
+
+#include <tag.h>
+#include <fileref.h>
+
+//
+// Releases the specified pointer if not NULL
+//
+#define SAFE_RELEASE(p)     \
+	if (p)                  \
+	{                       \
+		(p)->Release();     \
+		(p) = NULL;         \
+	}
+
+// DLL lifetime management functions
+void DllAddRef();
+void DllRelease();
+
+// Debug property handler class definition
+class CTagLibPropertyStore :
+	public IPropertyStore,
+	public IPropertyStoreCapabilities,
+	public IInitializeWithStream
+{
+public:
+	static HRESULT CreateInstance(REFIID riid, void **ppv);
+
+	// IUnknown
+	IFACEMETHODIMP QueryInterface(REFIID riid, void **ppv)
+	{
+		static const QITAB qit[] = {
+			QITABENT(CTagLibPropertyStore, IPropertyStore),
+			QITABENT(CTagLibPropertyStore, IPropertyStoreCapabilities),
+			QITABENT(CTagLibPropertyStore, IInitializeWithStream),
+			{ 0 },
+		};
+		return QISearch(this, qit, riid, ppv);
+	}
+
+	IFACEMETHODIMP_(ULONG) AddRef()
+	{
+		DllAddRef();
+		return InterlockedIncrement(&_cRef);
+	}
+
+	IFACEMETHODIMP_(ULONG) Release()
+	{
+		DllRelease();
+		ULONG cRef = InterlockedDecrement(&_cRef);
+		if (!cRef)
+		{
+			delete this;
+		}
+		return cRef;
+	}
+
+	// IPropertyStore
+	IFACEMETHODIMP GetCount(__out DWORD *pcProps);
+	IFACEMETHODIMP GetAt(DWORD iProp, __out PROPERTYKEY *pkey);
+	IFACEMETHODIMP GetValue(REFPROPERTYKEY key, __out PROPVARIANT *pPropVar);
+	IFACEMETHODIMP SetValue(REFPROPERTYKEY key, REFPROPVARIANT propVar);
+	IFACEMETHODIMP Commit();
+
+	// IPropertyStoreCapabilities
+	IFACEMETHODIMP IsPropertyWritable(REFPROPERTYKEY key);
+
+	// IInitializeWithStream
+	IFACEMETHODIMP Initialize(IStream *pStream, DWORD grfMode);
+
+protected:
+	CTagLibPropertyStore() : _cRef(1), _pStream(NULL), _grfMode(0)
+	{
+		DllAddRef();
+	}
+
+	~CTagLibPropertyStore()
+	{
+		SAFE_RELEASE(_pStream);
+	}
+
+	IStream*             _pStream; // data stream passed in to Initialize, and saved to on Commit
+	DWORD                _grfMode; // STGM mode passed to Initialize
+	TagLib::FileRef taglibfile;
+private:
+
+	long _cRef;
+};
+
+std::wstring name(REFPROPERTYKEY thing)
+{
+	wchar_t *str;
+	PSGetNameFromPropertyKey(thing, &str);
+	std::wstring ret(str);
+	CoTaskMemFree(str);
+	return ret;
+}
+
+bool operator==(REFPROPERTYKEY left, REFPROPERTYKEY right)
+{
+	return IsEqualPropertyKey(left, right);
+}
+
+HRESULT CTagLibPropertyStore::GetValue(REFPROPERTYKEY key, __out PROPVARIANT *pPropVar)
+{
+	const TagLib::AudioProperties *ap = taglibfile.audioProperties();
+	const TagLib::Tag *tag = taglibfile.tag();
+
+	if (ap && key == PKEY_Audio_ChannelCount)
+	{
+		pPropVar->uintVal = ap->channels();
+		pPropVar->vt = VT_UI4;
+	}
+	else if (ap && key == PKEY_Media_Duration)
+	{
+		pPropVar->ulVal = ap->length()*10000000;
+		pPropVar->vt = VT_UI8;
+	}
+	else if (ap && key == PKEY_Audio_EncodingBitrate)
+	{
+		pPropVar->uintVal = ap->bitrate()*1024;
+		pPropVar->vt = VT_UI4;
+	}
+	else if (ap && key == PKEY_Audio_SampleRate)
+	{
+		pPropVar->uintVal = ap->sampleRate();
+		pPropVar->vt = VT_UI4;
+	}
+	else if (tag && key == PKEY_Music_AlbumTitle)
+	{
+		pPropVar->vt = VT_BSTR;
+		pPropVar->bstrVal = SysAllocString(tag->album().toWString().c_str());
+	}
+	else if (tag && key == PKEY_Music_Artist)
+	{
+		pPropVar->vt = VT_BSTR;
+		pPropVar->bstrVal = SysAllocString(tag->artist().toWString().c_str());
+	}
+	else if (tag && key == PKEY_Music_Genre)
+	{
+		pPropVar->vt = VT_BSTR;
+		pPropVar->bstrVal = SysAllocString(tag->genre().toWString().c_str());
+	}
+	else if (tag && key == PKEY_Title)
+	{
+		pPropVar->vt = VT_BSTR;
+		pPropVar->pwszVal = SysAllocString(tag->title().toWString().c_str());
+	}
+	else if (tag && key == PKEY_Music_TrackNumber)
+	{
+		if (tag->track() == 0)
+			pPropVar->vt = VT_EMPTY;
+		else
+		{
+			pPropVar->uintVal = tag->track();
+			pPropVar->vt = VT_UI4;
+		}
+	}
+	else if (tag && key == PKEY_Media_Year)
+	{
+		if (tag->year() == 0)
+			pPropVar->vt = VT_EMPTY;
+		else
+		{
+			pPropVar->uintVal = tag->year();
+			pPropVar->vt = VT_UI4;
+		}
+	}
+	else
+		return S_FALSE;
+
+	return S_OK;
+}
+
+HRESULT CTagLibPropertyStore::CreateInstance(REFIID riid, void **ppv)
+{
+	HRESULT hr = E_OUTOFMEMORY;
+	CTagLibPropertyStore *pNew = new CTagLibPropertyStore();
+	if (pNew)
+	{
+		hr = pNew->QueryInterface(riid, ppv);
+		SAFE_RELEASE(pNew);
+	}
+
+	return hr;
+}
+
+HRESULT CTagLibPropertyStore_CreateInstance(REFIID riid, void **ppv)
+{
+	return CTagLibPropertyStore::CreateInstance(riid, ppv);
+}
+
+const PROPERTYKEY keys[] = {
+	PKEY_Music_AlbumTitle, PKEY_Music_Artist,
+	PKEY_Music_TrackNumber, PKEY_Music_Genre,
+	PKEY_Title, PKEY_Media_Year, PKEY_Audio_ChannelCount,
+	PKEY_Media_Duration, PKEY_Audio_EncodingBitrate, PKEY_Audio_SampleRate };
+
+HRESULT CTagLibPropertyStore::GetCount(__out DWORD *pcProps)
+{
+	return ARRAYSIZE(keys);
+}
+
+HRESULT CTagLibPropertyStore::GetAt(DWORD iProp, __out PROPERTYKEY *pkey)
+{
+	pkey = const_cast<PROPERTYKEY*>(&keys[iProp]);
+	return S_OK;
+}
+
+// SetValue just updates the internal value cache
+HRESULT CTagLibPropertyStore::SetValue(REFPROPERTYKEY key, REFPROPVARIANT propVar)
+{
+	return STG_E_ACCESSDENIED; // Ok, but read-only.
+}
+
+// Commit writes the internal value cache back out to the stream passed to Initialize
+HRESULT CTagLibPropertyStore::Commit()
+{
+	return S_OK; // Ok, we wrote all we could (nothing).
+}
+
+// Indicates whether the users should be able to edit values for the given property key
+// S_OK | S_FALSE
+HRESULT CTagLibPropertyStore::IsPropertyWritable(REFPROPERTYKEY key)
+{
+	return S_FALSE;
+}
+
+struct IStreamAccessor : public TagLib::FileAccessor
+{
+	IStreamAccessor(IStream *stream) : stream(stream) {}
+	IStream *stream;
+	bool isOpen() const
+	{
+		return true;
+	}
+
+	size_t fread(void *pv, size_t s1, size_t s2) const
+	{
+		ULONG read = 0;
+		stream->Read(pv, s1*s2, &read);
+		return read;
+	}
+
+	size_t fwrite(const void *,size_t,size_t)
+	{
+		return 0;
+	}
+
+	int fseek(long distance, int direction)
+	{
+		LARGE_INTEGER dist;
+		dist.QuadPart = distance;
+		return FAILED(stream->Seek(dist, direction, NULL)); // 0 on success.
+	}
+
+	void clearError()
+	{
+		// Uh oh.
+	}
+
+	long tell() const
+	{
+		ULARGE_INTEGER newpos;
+		LARGE_INTEGER dist = {};
+		stream->Seek(dist, STREAM_SEEK_CUR, &newpos);
+		return newpos.QuadPart;
+	}
+
+	int truncate(long length)
+	{
+		return -1; // error
+	}
+
+	TagLib::FileNameHandle name() const
+	{
+		STATSTG a;
+		if (FAILED(stream->Stat(&a, STATFLAG_DEFAULT)))
+			return TagLib::FileName("");
+		return TagLib::FileName(a.pwcsName);
+	}
+
+	bool readOnly() const
+	{
+		return true;
+	}
+
+};
+
+// Initialize populates the internal value cache with data from the specified stream
+// S_OK | E_UNEXPECTED | ERROR_READ_FAULT | ERROR_FILE_CORRUPT | ERROR_INTERNAL_ERROR
+HRESULT CTagLibPropertyStore::Initialize(IStream *pStream, DWORD grfMode)
+{
+	taglibfile = TagLib::FileRef(new IStreamAccessor(pStream));
+	if (taglibfile.isNull())
+		return ERROR_FILE_CORRUPT;
+	return S_OK;
+}
