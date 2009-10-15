2ea5d36aea849783cad1716f00d65c93caac25b8
Chris West (Faux) <git@goeswhere.com>
2009-10-15 19:56:31 +0000

    pkg script - drop winrar, update to modern 7z flags

--- pkg.bat
+++ pkg.bat
@@ -1,5 +1,6 @@
 rem @echo off
-if not exist pkg mkdir pkg
+rd /q /s pkg
+mkdir pkg
 
 set RELEASE=%1
 if "%RELEASE%" == "" set RELEASE=Debug
@@ -20,8 +21,6 @@ copy x64\%RELEASE%\TaglibHandler.pdb pkg\x64
 
 copy README.txt pkg\README.txt
 
-for /F "usebackq tokens=2* delims= " %%f in (`reg query "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\App Paths\WinRAR.exe" /v Path`) do set RARPATH="%%g\rar.exe"
-for /F "usebackq tokens=2* delims= " %%f in (`reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\WinRAR.exe" /v Path`) do set RARPATH="%%g\rar.exe"
 for /F "usebackq tokens=2* delims= " %%f in (`reg query "HKLM\SOFTWARE\7-Zip" /v Path`) do set ZPATH="%%g\7z.exe"
 
 set VERSION=unknown
@@ -30,6 +29,5 @@ for /F "usebackq" %%f in (`svnversion`) do set VERSION="r%%f"
 set OUTNAME=..\taglibhandler-%VERSION%
 
 cd pkg
-if exist %RARPATH% %RARPATH% a -av -r -m5 -s %OUTNAME% *
-if exist %ZPATH% %ZPATH% a -mx9 -mson -sfx7z.sfx -r %OUTNAME% *
+if exist %ZPATH% %ZPATH% a -mx9 -ms=on -sfx7z.sfx -r %OUTNAME% *
 cd ..
